cb669c13681dd3d135cb7c87927a4a08e8c27035
Chaeguevara <dfso2@naver.com>
2022-03-21 16:32:00 +0900

    Call and Transact "store" function in SimpleStorage based on Ganache GUI
    - get public address and private key from ganache GUI
    - put them in deply.py and .env file
    - To send tx
      1. create Tx
      2. Sign
      3. Send
      4. Wait
      5. print out result -> here "0" to "15"

--- web3_py_simple_storage/deploy.py
+++ web3_py_simple_storage/deploy.py
@@ -40,7 +40,7 @@ abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 #for connecting to ganash
 w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
 chain_id = 1337
-my_address = "0xf0735bF36aBFBA2e91a170e16BeF5870e80c3E44"
+my_address = "0x471bA332c93c0eA9b27Dab88F3AfB2C7f002EDA5"
 private_key = os.getenv("PRIVATE_KEY")
 
 #Create the contract in python
@@ -55,15 +55,36 @@ transaction = SimpleStorage.constructor().buildTransaction(
 )
 #2. Sign a Tx
 signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
-
+print("Deploying contract...")
 #3. Send a Tx
 tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
 #Code stop until response
 tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print("Deployed contract!")
+
 
 #working with Contract
 #contract addr
 #contract abi
 simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
 #call vs transact
-print(simple_storage.functions.retrieve())
+
+print(simple_storage.functions.retrieve().call())
+print("Updating contract...")
+#Create tx
+store_transaction = simple_storage.functions.store(15).buildTransaction(
+        {
+            "chainId":chain_id,"from":my_address,"nonce":nonce+1,"gasPrice":w3.eth.gas_price
+            })
+#sign transaction
+signed_store_txn=w3.eth.account.sign_transaction(
+        store_transaction,private_key=private_key)
+#send transaction
+send_store_tx = w3.eth.send_raw_transaction(signed_store_txn.rawTransaction)
+#Wait for transacted 
+tx_receip=w3.eth.wait_for_transaction_receipt(send_store_tx)
+print("Updated!")
+#show tx result
+print(simple_storage.functions.retrieve().call())
+
+
