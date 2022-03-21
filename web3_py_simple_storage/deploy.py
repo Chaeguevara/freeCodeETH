edb90fe6db610b01422fc9fc118b7662dd6d0459
Chaeguevara <dfso2@naver.com>
2022-03-21 16:42:26 +0900

    Connect to ganache-cli
    - run ganache-cli with "ganache -d". -d stands for fixed address and
      private key
    - update private key and public address in the python file and .env file
    - run python file and check the response in cli

--- web3_py_simple_storage/deploy.py
+++ web3_py_simple_storage/deploy.py
@@ -38,9 +38,10 @@ bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"
 abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 
 #for connecting to ganash
-w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
+w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
 chain_id = 1337
-my_address = "0x471bA332c93c0eA9b27Dab88F3AfB2C7f002EDA5"
+my_address = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"
+
 private_key = os.getenv("PRIVATE_KEY")
 
 #Create the contract in python
