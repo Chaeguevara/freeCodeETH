e04c3b3502a6a8b324f72309b50920fc6d1b5a7a
Chaeguevara <dfso2@naver.com>
2022-03-27 21:20:08 +0900

    Add interface functionality based on chainlink
    - add rinkeby address to .yaml
    - utilize .yaml to look for corresponding parameter for local and
      rinkeby test
    - test passed for local ganache
    - test passed for rinkeby testNet

--- brownie_fund_me/contracts/FundMe.sol
+++ brownie_fund_me/contracts/FundMe.sol
@@ -11,8 +11,11 @@ contract FundMe{
     mapping(address => uint256) public addressToAmountFunded;
     address[] public funders;
     address public owner;
-    constructor() public {
+    AggregatorV3Interface public priceFeed;
+
+    constructor(address _priceFeed) public {
         owner = msg.sender;
+        priceFeed = AggregatorV3Interface(_priceFeed);
     }
 
     function fund() public payable{
@@ -26,12 +29,10 @@ contract FundMe{
     }
 
     function getVersion() public view returns (uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
         return priceFeed.version();
     }
 
     function getPrice() public view returns(uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
         (
             ,int256 answer,,,
             
--- brownie_fund_me/scripts/deploy.py
+++ brownie_fund_me/scripts/deploy.py
@@ -1,10 +1,22 @@
-from brownie import accounts,config,network,FundMe
+from brownie import accounts,config,network,FundMe,MockV3Aggregator
 from scripts.helpful_scripts import get_account
 
 
 def deploy_fund_me():
     account = get_account()
-    fund_me = FundMe.deploy({"from":account}, publish_source=True)
+    if network.show_active() != "development":
+        price_feed_address = config["networks"][network.show_active()]["eth_usd_price_feed"]
+        
+    else:
+        print(f"The active network is {network.show_active()}")
+        print(f"Deply MockV3Aggregator...")
+        mock_aggregator=MockV3Aggregator.deploy(18,2000000000000000000000,{"from":account})
+        price_feed_address=mock_aggregator.address
+        print("Mock deployed!")
+
+
+    
+    fund_me = FundMe.deploy(price_feed_address, {"from":account}, publish_source=config["networks"][network.show_active()].get("verify"))
     print(f"Contract deployed to {fund_me.address}")
 
 def main():
