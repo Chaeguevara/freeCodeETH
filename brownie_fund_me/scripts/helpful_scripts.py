374f91e3c4949d89449914255114c9ef9a06846c
Chaeguevara <dfso2@naver.com>
2022-03-28 18:20:13 +0900

    Refrator parameters
     Refractor code and set up local blockchain as like TestNet to keep
     track of transaction
    - set up for ganache local blockchain
    - add ganache-local Ethereum net

--- brownie_fund_me/scripts/helpful_scripts.py
+++ brownie_fund_me/scripts/helpful_scripts.py
@@ -1,8 +1,22 @@
-from brownie import accounts,config,network
+from brownie import accounts,config,network,MockV3Aggregator
+from web3 import Web3
 
+DECIMALS=8
+STARTING_PRICE=2*(10**10)
+LOCAL_BLOCKCHAIN_ENVIRONMENTS=["development","ganache-local"]
 
 def get_account():
-    if(network.show_active()=="development"):
+    if(network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS):
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
+
+def deploy_mocks():
+    print(f"The active network is {network.show_active()}")
+    print("Deply MockV3Aggregator...")
+    if len(MockV3Aggregator) <= 0:
+        print("deploying")
+        MockV3Aggregator.deploy(DECIMALS,Web3.toWei(STARTING_PRICE,"ether"),{"from":get_account()})
+    print("Mock deployed!")
+
+    
