c3571508e047a83a407dc86dec5340d290b7464c
Chaeguevara <dfso2@naver.com>
2022-04-24 18:16:08 +0900

    Project end
    - some feature in useDapp doens't work

--- defi-stake-yield-brownie/front_end/src/components/yourWallet/WalletBalance.tsx
+++ defi-stake-yield-brownie/front_end/src/components/yourWallet/WalletBalance.tsx
@@ -1,24 +1,19 @@
-import { useEthers, useTokenBalance, useEtherBalance } from "@usedapp/core";
-import { formatUnits } from "ethers/lib/utils";
-import { Token } from "../Main";
-import { BalanceMsg } from "../BalanceMsg";
+import { Token } from "../Main"
+import { useEthers, useTokenBalance } from "@usedapp/core"
+import { formatUnits } from "@ethersproject/units"
+import { BalanceMsg } from "../../components/BalanceMsg"
 
 export interface WalletBalanceProps {
-  token: Token;
+    token: Token
 }
 
 export const WalletBalance = ({ token }: WalletBalanceProps) => {
-  const { image, address, name } = token;
-  const { account } = useEthers();
-  const tokenBalance = useTokenBalance(address, account);
-  const formattedTokenBalance: number = tokenBalance
-    ? parseFloat(formatUnits(tokenBalance, 18))
-    : 0;
-  return (
-    <BalanceMsg
-      label={`Your un-staked ${name} balance`}
-      tokenImgSrc={image}
-      amount={formattedTokenBalance}
-    />
-  );
-};
+    const { image, address, name } = token
+    const { account } = useEthers()
+    const tokenBalance = useTokenBalance(address, account)
+    const formattedTokenBalance: number = tokenBalance ? parseFloat(formatUnits(tokenBalance, 18)) : 0
+    return (<BalanceMsg
+        label={`Your un-staked ${name} balance`}
+        tokenImgSrc={image}
+        amount={formattedTokenBalance} />)
+}
